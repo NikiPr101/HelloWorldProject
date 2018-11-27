5956ece4567a99db571f6b0c7543d0c50cf1d090
NikiPr101 <45207515+NikiPr101@users.noreply.github.com>
2018-11-27 17:49:42 +0100

    Update HelloWorld.java
    Neuer Print befehl

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Klasse");
-		// das ist ein kommentar, kein Code, der ausgeführt wird
+		System.out.println("Hallo Nikolas Richard Adreas Preuler");
+		// das ist ein kommentar, kein Code, der ausgefÃ¼hrt wird
 		// das ist ein einzeiliger Kommentar
 		/*
 		 * Mehrzeiliger Kommentar
