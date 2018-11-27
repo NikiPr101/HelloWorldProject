4c757b64e8b21d4a58517859e7f0df3f1d2d7140
nikolas.preuler <nikolas.preuler@KK-GYM-EDV1-10.gym.kk.local>
2018-11-27 17:39:38 +0100

    kommentare hinzugefügt

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Klasse");
+		// das ist ein kommentar, kein Code, der ausgeführt wird
+		// das ist ein einzeiliger Kommentar
+		/*
+		 * Mehrzeiliger Kommentar
+		 */
 	}
 
 }
