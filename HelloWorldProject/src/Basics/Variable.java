5a40d098aea690a989a6f34409976e15066e39dc
nikolas.preuler <nikolas.preuler@KK-GYM-EDV1-10.gym.kk.local>
2018-12-18 18:05:18 +0100

    Update 2 18.12.2018

--- HelloWorldProject/src/Basics/Variable.java
+++ HelloWorldProject/src/Basics/Variable.java
@@ -12,11 +12,29 @@ public class Variable {
 		short shortVar;
 		System.out.println(Short.MIN_VALUE);
 		System.out.println(Short.MAX_VALUE);
+		//
 		shortVar = 2018;
 		System.out.println(shortVar);
-		shortVar = (short) (7 + shortVar);
+		//
+		shortVar = (short) (shortVar - 16);
 		System.out.println(shortVar);
-
+		//
+		System.out.println(9/4.0);
+		System.out.println(shortVar + 10);
+		//
+		//
+		int intVar;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		//
+		//
+		long longVar;
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		longVar = 1000*1000*1000*1000;
+		System.out.println(longVar);
+		//
+		System.out.println(System.currentTimeMillis());
 	}
 
 }
