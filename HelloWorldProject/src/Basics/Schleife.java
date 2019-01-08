386c6d189a77b38215b92485c65f3a94fff1c920
nikolas.preuler <nikolas.preuler@KK-GYM-EDV1-10.gym.kk.local>
2019-01-08 18:03:24 +0100

    Update Schleife

--- HelloWorldProject/src/Basics/Schleife.java
+++ HelloWorldProject/src/Basics/Schleife.java
@@ -13,7 +13,7 @@ public class Schleife {
 			System.out.println("Zahl ist "+ i);
 		}
 		//Schleive von 100 bis 0
-		for (int i=100; i > 0; i= i-1) {
+		for (int i=100; i > 1; i= i-1) {
 			System.out.println("Countdown bei: "+ i);
 		}
 	}
